d6cb0f00fe9706a2fa2fbf4236c5cfc7daf2a583
nemethdavid03 <nmthdvd123@gmail.com>
2025-06-19 18:32:36 +0200

    fix client

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,4 +1,7 @@
-import ClientSuccess from "./components/ClientSuccess";
+
+import dynamic from "next/dynamic";
+
+const ClientSuccess = dynamic(() => import("./components/ClientSuccess"), { ssr: false });
 
 export default function SuccessPage() {
     return <ClientSuccess />;
